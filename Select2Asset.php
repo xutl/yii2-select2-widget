c1b5219fa8ae69a7e2b7edc776a11d1faefd3cec
xutl <79109681@qq.com>
2016-10-24 14:18:40 +0800

    init

---
+++ Select2Asset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\select2;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * Class Select2Asset
+ * @package xutl\ueditor
+ */
+class Select2Asset extends AssetBundle
+{
+    public $sourcePath = '@vendor/xutl/yii2-select2-widget/assets';
+
+    public $js = [
+        'select2.min.js',
+    ];
+
+    public $css = [
+        'css/select2.min.css',
+		'css/select2-bootstrap.min.css',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
